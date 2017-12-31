6e979caaafe2374833dcf3e154c7e5a0dc73237a
eikhantmon <eikhantmon961@gmail.com>
2017-12-31 10:56:25 +0630

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/employer', function(Request $request) {
+	if ($request->user()->hasRole('manager')) {
+		return redirect('/home');
+	}
+
+	return 'Welocme';
+});
